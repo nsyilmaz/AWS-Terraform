220d4ac264f7b6b35d2492dc364c754864625464
Nebi Senol YILMAZ <nsyilmaz@gmail.com>
2023-06-25 16:27:09 +0100

    priv

--- 01-Basic-Instance/05-Instances.tf
+++ 01-Basic-Instance/05-Instances.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "VPN" {
+resource "aws_instance" "PUB-SRV-1" {
     ami                         = "ami-09e67e426f25ce0d7"
     instance_type               = "t2.micro"
     associate_public_ip_address = true
@@ -12,19 +12,28 @@ resource "aws_instance" "VPN" {
     ]
 
 
+    user_data = "${file("InstallApache.sh")}"
+
+
     tags = {
-        Name = "VPN"
+        Name = "PUB-SRV-1"
     }
+
+
+    depends_on = [
+        aws_route_table_association.public-1
+    ]
+
 }
 
 
-resource "aws_instance" "POC" {
+resource "aws_instance" "PUB-SRV-2" {
     ami                         = "ami-09e67e426f25ce0d7"
     instance_type               = "t2.micro"
     associate_public_ip_address = true
-    subnet_id                   = aws_subnet.subnet-pub-2.id
+    subnet_id                   = aws_subnet.subnet-pub-1.id
     key_name                    = "AWS"
-    private_ip                  = "172.30.2.5"
+    private_ip                  = "172.30.1.10"
     source_dest_check           = false
 
     vpc_security_group_ids      = [
@@ -32,16 +41,27 @@ resource "aws_instance" "POC" {
     ]
 
 
+    user_data = "${file("InstallApache.sh")}"
+
+
     tags = {
-        Name = "POC"
+        Name = "PUB-SRV-2"
     }
+
+
+    depends_on = [
+        aws_route_table_association.public-1
+    ]
+
 }
 
 
-resource "aws_instance" "InternalFileServer" {
+
+
+resource "aws_instance" "PRIV-SRV-1" {
     ami                         = "ami-09e67e426f25ce0d7"
     instance_type               = "t2.micro"
-    associate_public_ip_address = false
+    associate_public_ip_address = true
     subnet_id                   = aws_subnet.subnet-priv-1.id
     key_name                    = "AWS"
     private_ip                  = "172.30.10.5"
@@ -52,59 +72,16 @@ resource "aws_instance" "InternalFileServer" {
     ]
 
 
-    tags = {
-        Name = "InternalFileServer"
-    }
-}
-
-
-resource "aws_instance" "WebServer-1" {
-    ami                         = "ami-09e67e426f25ce0d7"
-    instance_type               = "t2.micro"
-    associate_public_ip_address = false
-    subnet_id                   = aws_subnet.subnet-priv-1.id
-    key_name                    = "AWS"
-    private_ip                  = "172.30.10.10"
-
-    vpc_security_group_ids      = [
-        aws_security_group.Allow-SSH-HTTP.id,
-    ]
-
-
     user_data = "${file("InstallApache.sh")}"
 
 
     tags = {
-        Name = "WebServer-1"
+        Name = "PRIV-SRV-1"
     }
+
 
     depends_on = [
         aws_route_table_association.private-1
     ]
-}
 
-
-resource "aws_instance" "WebServer-2" {
-    ami                         = "ami-09e67e426f25ce0d7"
-    instance_type               = "t2.micro"
-    associate_public_ip_address = false
-    subnet_id                   = aws_subnet.subnet-priv-2.id
-    key_name                    = "AWS"
-    private_ip                  = "172.30.20.10"
-
-    vpc_security_group_ids      = [
-        aws_security_group.Allow-SSH-HTTP.id,
-    ]
-
-
-    user_data = "${file("InstallApache.sh")}"
-
-
-    tags = {
-        Name = "WebServer-2"
-    }
-
-    depends_on = [
-        aws_route_table_association.private-2
-    ]
 }
